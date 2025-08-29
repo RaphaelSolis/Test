40c311c4524e13fdebf055e3693eefdc3ae1c32a
Raphael Solisgit config --global user.email raphaelsolis9@gmail.comgit config --global user.name Raphael <raphaelsolis9@gmail.com>
2025-08-29 17:24:35 +0000

    Clean start

---
+++ Main.java
@@ -0,0 +1,81 @@
+public class Main {
+    public static void main(String[] args) {
+        //Raphael A. Solis
+        //BSIS-2
+        System.out.println("Title: Said I loved you but I lied");
+        System.out.println("By Michael Bolton - 1993");
+        System.out.println("Genre: Adult contemporary, rock");
+        System.out.println("----------------------------------");
+        System.out.println("            Lyrics");
+        //Verse 1
+         System.out.println("Ooh-ooh, ooh\n" + 
+         "You are the candle, love's the flame\n" +
+         "Shine your light on this heart of mine\n" +
+         "'Til the end of time\n" +
+         "You came to me like the dawn through the night\n" +
+         "Just shinin' like the sun\n" +
+         "Out of my dreams and into my life\n" +
+         "You are the one, you are the one\n"
+         );
+
+         //Chorus
+         System.out.println("Said I loved you, but I lied\n" +
+         "Cause this is more than love I feel inside\n" +
+         "Said I loved you, but I was wrong\n" +
+         "'Cause love could never ever feel so strong\n" +
+         "Said I loved you but I lied\n"
+         );
+
+         //Verse 2
+         System.out.println("With all my soul, I've tried in vain\n" + 
+         "How can mere words this heart explain\n" +
+         "This taste of heaven so deep, so true\n" +
+         "I've found in you\n" +
+         "So may reasons in so many ways\n" +
+         "My life has just begun\n" +
+         "I need you forever, well, I need you to stay\n" +
+         "You are the one, you are the one\n"
+         );
+
+         //Chorus
+         System.out.println("Said I loved you, but I lied\n" +
+         "Cause this is more than love I feel inside\n" +
+         "Said I loved you, but I was wrong\n" +
+         "'Cause love could never ever feel so strong\n" +
+         "Said I loved you but I lied\n"
+         );
+
+         //Bridge
+         System.out.println("You came to me like the dawn through the night\n" +
+         "Just shinin', shinin' like the sun\n" +
+         "Out of my dreams and into my life\n" +
+         "You are the one, you are the one\n"
+         );
+
+         //Chorus
+         System.out.println("Said I loved you, but I lied (Said I loved you but I lied)\n" +
+         "'Cause this is more than love I feel inside (Said I loved you but I lied\n" +
+         "Said I loved you, but I was wrong (Said I loved you but I'm wrong\n" +
+         "'Cause love could never ever feel so strong\n" +
+         "(Ooh)\n"
+         );
+         //Outro
+         System.out.println("Said I loved you (Said I loved you)\n" +
+         "But this is more than love I feel inside\n" +
+         "Said I loved you but I lied\n");
+
+         // 2 persons inside the heart
+          
+        System.out.println("  ***     ***  ");
+        System.out.println(" *****   ***** ");
+        System.out.println("******* *******");
+        System.out.println("***************");
+        System.out.println(" ***** o   o **");
+        System.out.println("  ****/|\\ /|\\**");
+        System.out.println("   ***/ \\ / \\**");
+        System.out.println("    *******    ");
+        System.out.println("     *****     ");
+        System.out.println("      ***      ");
+        System.out.println("       *       ");
+    }
+}
